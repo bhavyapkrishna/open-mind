67ffed30a8b2a1bf2f389a8116a790943533656d
bhavyapk <bhavyapkrishna@icloud.com>
2023-03-14 09:18:54 -0700

    Initial Commit

---
+++ Open Mind Prototype/Open_Mind_PrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Open_Mind_PrototypeApp.swift
+//  Open Mind Prototype
+//
+//  Created by Bhavya Krishna on 3/14/23.
+//
+
+import SwiftUI
+
+@main
+struct Open_Mind_PrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
